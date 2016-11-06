0bb8b33a2098eeb8a20099a06fbeb9c408daa78a
Soundararajan <soundararajan@usa.net>
2016-11-06 09:50:17 -0800

    heroku setup

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
  * Created by soundararajanvenkatasubramanian on 11/2/16.
  */
 var express = require('express');
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 // create our app
 
 var app = express();
+app.use( function (req, res, next){
+   if(req.headers['x-forwarded-proto'] === 'http'){
+      next();
+   }else{
+      res.redirect('http://' + req.hostname + req.url);
+   }
+});
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
    console.log(`Server is listening on port: ${PORT}`);
 });
+
